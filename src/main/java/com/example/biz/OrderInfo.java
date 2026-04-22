595e483b5a4b096f9bd08e8d660c1d04c05a25bb
bradyang <ibrady@foxmail.com>
2026-04-22 19:10:47 +0800

    feat: 添加订单查询skill

---
+++ src/main/java/com/example/biz/OrderInfo.java
@@ -0,0 +1,24 @@
+package com.example.biz;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Brady (brady@sfmail.sf-express.com)
+ * @version 1.0
+ * @since 2026/4/22 下午5:16
+ **/
+@Data
+public class OrderInfo {
+    private String goodsOwnerName;
+    private List<Material> materialList;
+
+    @Data
+    public static class Material {
+        private String materialName;
+        @JsonAlias("associatedPackageQuantity")
+        private Integer piece;
+    }
+}
